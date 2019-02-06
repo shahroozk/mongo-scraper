dbb8b45a9da71e4111d678367fc6815ef0ca5659
shahroozk <shahrooz.kanani@gmail.com>
2019-02-05 20:18:34 -0800

    edit some files

--- config/connection.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require("mongoose");
-
- //Local Database Configuration with Mongoose
-//  mongoose.connect("mongodb://localhost/goodnews", function(error)
-//  	{if(error) throw error;
-//  	console.log("Database connected");
-//  });
-mongoose.connect("mongodb://localhost/goodnews", { useNewUrlParser: true });
